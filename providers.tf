cc0607ac5e946c07cc4f9f2bb3aaecdb395943b4
eolver <46315332+eolver@users.noreply.github.com>
2019-03-05 20:25:21 +1100

    Add files via upload

---
+++ providers.tf
@@ -0,0 +1,6 @@
+provider "azurerm" {
+subscription_id = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+client_id = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+client_secret = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+tenant_id = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+}
